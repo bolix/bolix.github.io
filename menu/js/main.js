13b8bde62eabe401edbdd746b4ef26a5c065b113
bolix <bolix@live.com>
2014-05-10 02:12:10 -0400

    Se cambia para mostrar los detalles de los productos

--- menu/js/main.js
+++ menu/js/main.js
@@ -4,6 +4,18 @@ $(function  () {
 
 	json.Products = [];
 
+	var CountryListScript = $("#country-list-template").html();
+
+//Compile the template
+	var CountryList = Handlebars.compile (CountryListScript);
+
+	var ProductListScript = $("#product-list-template").html();
+	var ProductScript = $("#product-template").html();
+
+	//Compile the template
+		var ProductList = Handlebars.compile (ProductListScript);
+	var Product = Handlebars.compile (ProductScript);
+
 	$.get("data/data.csv", function(respons) {
      data = respons;
 	}).done(function() {
@@ -14,19 +26,11 @@ $(function  () {
 
   //var shoesData = [{name:"Nike", price:199.00 }, {name:"Loafers", price:59.00 }, {name:"Wing Tip", price:259.00 }];
    //Get the HTML from the template   in the script tag
-    var CountryListScript = $("#country-list-template").html();
 
-   //Compile the template
-    var CountryList = Handlebars.compile (CountryListScript);
 	var paises = [];
 	var pais = null;
 
-	var ProductListScript = $("#product-list-template").html();
-	var ProductScript = $("#product-template").html();
 
-   //Compile the template
-    var ProductList = Handlebars.compile (ProductListScript);
-	var Product = Handlebars.compile (ProductScript);
 	var productlist = [];
 	var product = [];
 	var paish = null;
@@ -54,8 +58,6 @@ $(function  () {
 			alert('Error cargando data: ' + e.message);
 		}}
 	);
-
-    //$.UISearch({articleID:'#paises',placeholder:'Search',results: 5});
 
 //We pass the shoesData object to the compiled handleBars function
 // The function will insert all the values from the objects in their respective places in the HTML and returned HTML as a string. Then we use jQuery to append the resulting HTML string into the page

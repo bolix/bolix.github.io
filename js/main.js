adbf22370b9ab7879696f18b287d47d46d9ecf12
bolix <bolix@live.com>
2014-05-26 20:57:41 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -46,6 +46,14 @@ function hideTwitterBoxElements() {
 }
 
 // somewhere in your code after html page load
-hideTwitterBoxElements();    
+hideTwitterBoxElements();
+
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-51363036-1', 'culturacervecera.do');
+  ga('send', 'pageview');
 
 });
